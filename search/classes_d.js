baa157b5bd147d8957de7e6071dbd03e9d136a50
me <Mizugola@gmail.com>
2017-10-08 17:45:15 -0400

    Updated documentation

--- search/classes_d.js
+++ search/classes_d.js
@@ -4,5 +4,6 @@ var searchData=
   ['scenenode',['SceneNode',['../classobe_1_1_transform_1_1_scene_node.html',1,'obe::Transform']]],
   ['screenstruct',['ScreenStruct',['../structobe_1_1_transform_1_1_screen_struct.html',1,'obe::Transform']]],
   ['selectable',['Selectable',['../classobe_1_1_types_1_1_selectable.html',1,'obe::Types']]],
-  ['soundwrapper',['SoundWrapper',['../classobe_1_1_sound_1_1_sound_wrapper.html',1,'obe::Sound']]]
+  ['soundwrapper',['SoundWrapper',['../classobe_1_1_sound_1_1_sound_wrapper.html',1,'obe::Sound']]],
+  ['sprite',['Sprite',['../classobe_1_1_graphics_1_1_sprite.html',1,'obe::Graphics']]]
 ];
