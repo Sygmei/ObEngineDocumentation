28b282594eda1ad3a2bad1f7e6f6a67ccf309d0f
me <Mizugola@gmail.com>
2017-10-31 12:47:14 -0400

    Updated documentation

--- search/all_6.js
+++ search/all_6.js
@@ -12,7 +12,6 @@ var searchData=
   ['findsubvector',['findSubVector',['../namespacevili_1_1_functions_1_1_vector.html#acd48da64ad5d8c23cb96662066d30ac3',1,'vili::Functions::Vector']]],
   ['first',['first',['../classvili_1_1_node_iterator.html#a5f94edec5ec7388f8c952978e738a9db',1,'vili::NodeIterator']]],
   ['float',['Float',['../unionvili_1_1_data_node_1_1_data.html#ab1ebb8c873c278e479ae05f75ba27c4f',1,'vili::DataNode::Data::Float()'],['../namespacevili.html#ad7068bc2e5364f41b58e140dafbb1e1fa22ae0e2b89e5e3d477f988cc36d3272b',1,'vili::Float()']]],
-  ['font',['font',['../classobe_1_1_graphics_1_1_canvas_1_1_text.html#a84edc487605e4b52e04f58903e05d4a2',1,'obe::Graphics::Canvas::Text']]],
   ['fontloader',['fontLoader',['../namespaceobe_1_1_system_1_1_loaders.html#a24ca176b9fa8b2b58ae513e35fc70a69',1,'obe::System::Loaders']]],
   ['fontpath',['fontPath',['../classobe_1_1_graphics_1_1_canvas_1_1_text.html#a2c14c45e180ab9cf626cbfd3e03134c2',1,'obe::Graphics::Canvas::Text']]],
   ['force',['Force',['../namespaceobe_1_1_animation.html#a346c6729e9af989f6b063e6086637fbba9eb6b78a99cdb6ffd3d40d18621d9f80',1,'obe::Animation']]],
--- search/variables_f.js
+++ search/variables_f.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['text',['text',['../classobe_1_1_graphics_1_1_canvas_1_1_text.html#ac8af56faa46dfac341c80497fe766eca',1,'obe::Graphics::Canvas::Text']]],
   ['textureloader',['textureLoader',['../namespaceobe_1_1_system_1_1_loaders.html#afdc80167d0e9daae49bcf31a53af0dd1',1,'obe::System::Loaders']]],
   ['thickness',['thickness',['../classobe_1_1_graphics_1_1_canvas_1_1_line.html#a4f12c3bf35b8c03b81c719db18a93100',1,'obe::Graphics::Canvas::Line']]],
   ['timestamped',['Timestamped',['../classobe_1_1_debug_1_1_console.html#a63abd459cae48d1b530376862ccf7495',1,'obe::Debug::Console']]],
